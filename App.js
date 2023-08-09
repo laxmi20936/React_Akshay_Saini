0b75a908c45f3fd64cb2fbdee7ef0b7825c2042f
laxmi <waghmorelaxmi@gmail.com>
2023-08-09 21:13:17 +0530

    epi3

---
+++ App.js
@@ -0,0 +1,11 @@
+const App = () =>{
+    return (
+        <div className="header">
+            <img className="user"src="https://static.vecteezy.com/system/resources/previews/004/312/696/original/food-logo-template-design-icon-illustration-vector.jpg" alt="logo"/>
+            <input className="input" type="text"/>
+            <img className="logo" src="https://static.vecteezy.com/system/resources/previews/005/005/788/original/user-icon-in-trendy-flat-style-isolated-on-grey-background-user-symbol-for-your-web-site-design-logo-app-ui-illustration-eps10-free-vector.jpg" alt="user"/>
+        </div>
+    )
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App';
+import "./index.css"
 
-const heading = React.createElement("h1",{className:"head"},"I am React")
+const heading = React.createElement("div",{className:"title"},
+[React.createElement("h1",{},"this is h1 tag"),
+React.createElement("h2",{}, "this is h2 tag"),
+React.createElement("h3",{},"this is h3 tag")])
 console.log(heading); 
 
-const jsxHead = <h1>Namo Namo</h1>
-console.log(jsxHead); 
+const jsxHead = (<div className='ok'>
+                    <h1>this is JSX h1</h1>
+                    <h2>this is JSX h2</h2>
+                    <h3>this is JSX h3</h3>
+</div>)
+
+function Title (){
+    return <div>I am Title</div>
+}
+
+const JsxFuctionalHead = () => (<div className='abc'>
+    <h1>this is Functional JSX h1</h1>
+    <h2>this is Functional JSX h2</h2>
+    <h3>this is  Functional JSX h3</h3>
+    <Title/>
+</div>)
+
+console.log(JsxFuctionalHead())
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(jsxHead)
+root.render(<App/>)
 
